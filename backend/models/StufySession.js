f1427c8695538328fbad5c8ddb4387a5d8519bdc
Rappykyun <rakuichijo155@gmail.com>
2024-11-04 09:11:27 +0800

    added model for studySessions

---
+++ backend/models/StufySession.js
@@ -0,0 +1,75 @@
+const mongoose = require("mongoose");
+
+const studySessionSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  course: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Course",
+  },
+  category: {
+    type: String,
+    enum: [exam_prep, homework, reeding, review, practice],
+    required: true,
+  },
+  startTime: {
+    type: Date,
+    required: true,
+  },
+  endTime: {
+    type: Date,
+    required: true,
+  },
+  description: String,
+  recurring: {
+    enabled: {
+      type: Boolean,
+      Default: false,
+    },
+    frequency: {
+      type: String,
+      enum: ["daily", "weekly", "monthly"],
+      required: function () {
+        return this.recurring.enabled;
+      },
+    },
+    endDate: {
+      type: Date,
+      required: function () {
+        return this.recurring.enabled;
+      },
+    },
+  },
+  remainder: {
+    enabled: {
+      type: Boolean,
+      default: false,
+    },
+    time: {
+      type: Number,
+      default: 30,
+      required: function () {
+        return this.remainder.enabled;
+      },
+    },
+  },
+  progress: {
+    status: {
+      type: String,
+      enum: ["not_started", "in_progress", "completed", "missed"],
+      default: "not_started",
+      },
+      completedAt: Date,
+      duration: Number,
+      notes: String
+  },
+});
+
+export default mongoose.model("StudySession", studySessionSchema);
